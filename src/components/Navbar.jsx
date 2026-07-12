6e5ae14ec3ba05509eebf179b8d34f7690cfb496
sweety62 <myselfsweety1@gmail.com>
2026-07-12 17:09:24 +0530

    feat: enhance Navbar with mobile menu functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
     <div className="shadow-md sticky top-0 z-50 backdrop-blur-3xl">
 
       <div className="container mx-auto px-6 sm:px-8 md:px-12 lg:px-24">
+
         <div className="flex justify-between items-center py-8">
           {/*logo */}
           <div className="flex text-center text-2xl font-bold">
@@ -20,12 +21,11 @@ const Navbar = () => {
           </div>
           {/* Nav links(hidden on small screen) */}
           <nav className=" hidden md:flex items-center space-x-6 text-dark">
-            <a className="font-semibold hover:text-primary" href="#home">Home</a>
-            <a className="font-semibold hover:text-primary" href="#about">About</a>
-            <a className="font-semibold hover:text-primary" href="#menu">Menu</a>
-            <a className="font-semibold hover:text-primary" href="#reservation">Reservation</a>
-            <a className="font-semibold hover:text-primary" href="#contact">Contact</a>
-
+          <a   className="font-semibold hover:text-primary" href='#home'>Home</a>
+          <a className="font-semibold hover:text-primary" href='#about'>About</a>
+          <a className="font-semibold hover:text-primary" href='#menu'>Menu</a>
+          <a className="font-semibold hover:text-primary" href='#reservation'>Reservation</a>
+          <a className="font-semibold hover:text-primary" href='#contact'>Contact</a>
           </nav>
           {/* Mobile menu  button(visible on mobile screen) */}
           <div className=' block md:hidden'>
@@ -44,6 +44,19 @@ const Navbar = () => {
         </div>
 
       </div>
+      {
+        showMenu && (
+          <div className='md:hidden flex flex-col items-center space-y-6 py-20 h-screen' >
+            
+              <a onClick={()=>setShowMenu(prev => !prev)} className="font-semibold hover:text-primary" href='#home'>Home</a>
+              <a onClick={()=>setShowMenu(prev => !prev)}className="font-semibold hover:text-primary" href='#about'>About</a>
+              <a onClick={()=>setShowMenu(prev => !prev)} className="font-semibold hover:text-primary" href='#menu'>Menu</a>
+              <a onClick={()=>setShowMenu(prev => !prev)} className="font-semibold hover:text-primary" href='#reservation'>Reservation</a>
+              <a onClick={()=>setShowMenu(prev => !prev)} className="font-semibold hover:text-primary" href='#contact'>Contact</a>
+            
+          </div>
+        )
+      }
       
       
     </div>
