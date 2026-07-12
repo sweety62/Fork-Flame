ae780ef714652b52387042de2f275d3def5ba015
sweety62 <myselfsweety1@gmail.com>
2026-07-12 13:29:25 +0530

    feat: add Navbar component and mobile menu functionality
    - Created Navbar component with responsive design
    - Added mobile menu toggle functionality
    - Integrated Font and Tailwind CSS styles
    - Set up Vite configuration for React and Tailwind CSS
    - Added Home page component
    - Included Vite logo SVG asset

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import  { useState } from 'react'
+//import { FaXmark } from 'react-icons/fa6';
+import { FaBars } from 'react-icons/fa6';
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  console.log(showMenu);
+  return (
+    <div className="shadow-md sticky top-0 z-50 backdrop-blur-3xl">
+
+      <div className="container mx-auto px-6 sm:px-8 md:px-12 lg:px-24">
+        <div className="flex justify-between items-center py-8">
+          {/*logo */}
+          <div className="flex text-center text-2xl font-bold">
+            <img className="w-12 h-12 " src="./logo2.png" alt="Logo"/>
+            <h1 className="text-primary">
+              Fork&<span className="text-accent">Flame</span>
+            </h1>
+
+          </div>
+          {/* Nav links(hidden on small screen) */}
+          <nav className=" hidden md:flex items-center space-x-6 text-dark">
+            <a className="font-semibold hover:text-primary" href="#home">Home</a>
+            <a className="font-semibold hover:text-primary" href="#about">About</a>
+            <a className="font-semibold hover:text-primary" href="#menu">Menu</a>
+            <a className="font-semibold hover:text-primary" href="#reservation">Reservation</a>
+            <a className="font-semibold hover:text-primary" href="#contact">Contact</a>
+
+          </nav>
+          {/* Mobile menu  button(visible on mobile screen) */}
+          <div className=' block md:hidden'>
+            {
+              showMenu ? (
+                <h1 onClick={()=>setShowMenu(prev => !prev)} className='text-xl cursor-pointer' >
+                X
+                </h1>
+              ) : (
+                <FaBars onClick={()=>setShowMenu(prev => !prev)} className='text-xl cursor-pointer' />
+              )
+            }
+
+          </div>
+
+        </div>
+
+      </div>
+      
+      
+    </div>
+  )
+}
+
+export default Navbar
